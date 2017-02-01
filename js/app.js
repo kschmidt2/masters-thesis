031809cc7ee89c013c230d7621dd7108bda21c27
kschmidt2 <k.schmidt2@gmail.com>
2017-01-31 20:47:09 -0500

    adds scrollmagic

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
 $(document).foundation();
+
+// ScrollMagic
+var controller = new ScrollMagic.Controller();
+
+var scene = new ScrollMagic.Scene({
+  triggerElement: '#pinned-trigger1', // starting scene, when reaching this element
+  duration: 400 // pin the element for a total of 400px
+})
+.setPin('#header');
+
+controller.addScene(scene);
