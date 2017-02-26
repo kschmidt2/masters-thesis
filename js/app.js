990805d038a73196e89583eb6dd6fac04f473a96
kschmidt2 <k.schmidt2@gmail.com>
2017-02-26 17:56:01 -0500

    adds scroller functionality

--- js/app.js
+++ js/app.js
@@ -1,12 +1,11 @@
 $(document).foundation();
 
 // ScrollMagic
-var controller = new ScrollMagic.Controller();
-
-var scene = new ScrollMagic.Scene({
-  triggerElement: '#pinned-trigger1', // starting scene, when reaching this element
-  duration: 400 // pin the element for a total of 400px
-})
-.setPin('#header');
-
-controller.addScene(scene);
+// var controller = new ScrollMagic.Controller();
+//
+// var scene = new ScrollMagic.Scene({
+//   triggerElement: '#scene1', // starting scene, when reaching this element
+// })
+// .setClassToggle("#gwbridge", "appear")
+//
+// controller.addScene(scene);
